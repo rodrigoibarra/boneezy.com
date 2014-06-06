d78393f96503ae34d51978c8c4b9b6181427fafc
rodrigoibarra <hola@rodrigo-ibarra.com>
2014-06-06 12:22:39 -0500

    bare bones jekyll install and grunt, about to start development

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      build: {
+        src: ['dev/libs/*.js','dev/js/global.js'],
+        dest: '/build/js/global.min.js'
+      }
+    },
+      compass: {                  // Task
+        dist: {                   // Target
+          options: {              // Target options
+            sassDir: 'dev/scss/',
+            cssDir: 'build/css/',
+            environment: 'production'
+          }
+        }
+      },
+
+      watch: {
+          css: {
+            files: ['**/*.scss', '*.html'],
+            tasks: ['compass'],
+            options: {
+              livereload: true,
+            },
+          },
+      },
+
+      connect: {
+        uses_defaults: {}
+      }
+
+  });
+
+  // Load the plugin that provides the tasks.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  // Default task(s).
+  grunt.registerTask('default', ['uglify', 'compass']);
+
+};
