ce7b455e123447b9d9eaa910f4d5175e73ccf9f6
rodrigoibarra <hola@rodrigo-ibarra.com>
2018-01-30 13:04:05 -0600

    erased a lot of stuff im not really using anymore

--- Gruntfile.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(grunt) {
-
-  require('load-grunt-config')(grunt);
-  require("load-grunt-tasks")(grunt);
-
-  grunt.registerTask("default", ['compass:dev', "watch"]);
-  grunt.registerTask("deploy", ['compass:dist', 'imagemin']);
-};
