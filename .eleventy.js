394187b050be4c30a298966a0d08fcd37ed0cf78
rodrigoibarra <hola@rodrigo-ibarra.com>
2023-02-09 15:40:10 -0600

    starting point set

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,10 @@
 // Here we hecking go
-module.exports = function(eleventyConfig){
-    return {
-        markdownTemplateEngine: 'njk',
-        dataTemplateEngine: 'njk',
-        htmlTemplateEngine: 'njk',
-        dir: {
-          input: 'src',
-          output: 'dist',
-          includes: "_includes",
-          layouts: "layouts"
-        }
-      };
+module.exports = function () {
+  return{
+    dir:{
+      input: "src",
+      output: "dist",
+    },
+  };
+
 };
---
+++ src/index.md
@@ -0,0 +1,6 @@
+---
+layout: "base.njk"
+title: Beans & Pijamas
+---
+
+Is this working?
