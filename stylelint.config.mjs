0f9ff2fadd52e9b8ce183c8893292ddffdf7f3b7
Rodrigo Ibarra <rodrigoibarra@Rodrigos-MacBook-Pro.local>
2025-11-25 19:28:08 -0600

    Light layout and typesetting
    Currently the note layout is being lightly redesigned and typeset, looks
    and reads much better.

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -2,7 +2,7 @@
 export default {
   "extends": ["stylelint-config-standard"],
   "rules": {
-    "unit-allowed-list": ["px", "rem", "%", "ch", "deg", "vh", "fr"],
+    "unit-allowed-list": ["px", "rem", "%", "ch", "deg", "vh", "vw", "fr"],
     "value-keyword-case": ["lower", { "camelCaseSvgKeywords": true }],
     "no-invalid-position-at-import-rule": true,
     "no-duplicate-at-import-rules": true
