65f16ac0ad0c182d7a9ccb4a64f7392ad1ea7b6e
Rodrigo Ibarra <rodrigoibarra@Rodrigos-MacBook-Pro.local>
2025-11-29 15:58:37 -0600

    Landed on final typeset
    Final typeset ready, needs more layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://rodrigoibarra.rocks",
   integrations: [mdx(), sitemap()],
+  adapter: netlify(),
+  server: {
+    host: true,
+  },
 });
